bf6c4f3611d6a39774ab227b938e14552b1bc2b9
nifle3 <nifle3123@gmail.com>
2025-12-06 21:31:08 +0300

    complete second day

--- common/utils.cc
+++ common/utils.cc
@@ -1,7 +1,9 @@
 #include "utils.hpp"
 
 #include <fstream>
+#include <iterator>
 #include <optional>
+#include <ranges>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -20,4 +22,45 @@ get_lines_from_file(std::string_view file_name) noexcept {
   }
 
   return lines;
+}
+
+std::optional<std::string> read_file(std::string_view file_name) noexcept {
+  std::ifstream file(file_name.data());
+  if (!file) {
+    return std::nullopt;
+  }
+  return std::string(std::istreambuf_iterator<char>(file), {});
+}
+
+std::optional<std::vector<std::string>>
+read_file_with_delimeter(std::string_view file_name,
+                         std::string_view delimeter) noexcept {
+
+  auto content = read_file(file_name);
+  if (!content.has_value()) {
+    return std::nullopt;
+  }
+
+  return content.value() | std::views::split(delimeter) |
+         std::views::transform([](auto &&subrange) {
+           return std::string(subrange.begin(), subrange.end());
+         }) |
+         std::ranges::to<std::vector>();
+}
+
+std::size_t str_to_sizet(std::string_view str) {
+  str.remove_prefix(std::min(str.find_first_not_of(" \t\r\n"), str.size()));
+  str.remove_suffix(str.size() - str.find_last_not_of(" \t\r\n") - 1);
+  std::size_t result;
+  auto [ptr, err] =
+      std::from_chars(str.data(), str.data() + str.size(), result);
+  if (err == std::errc::result_out_of_range) {
+    throw std::out_of_range{"overflow"};
+  }
+
+  if (err != std::errc{} || ptr != (str.data() + str.size())) {
+    throw std::invalid_argument{"bad number"};
+  }
+
+  return result;
 }
--- common/utils.hpp
+++ common/utils.hpp
@@ -6,3 +6,11 @@
 
 std::optional<std::vector<std::string>>
 get_lines_from_file(std::string_view file_name) noexcept;
+
+std::optional<std::string> read_file(std::string_view file_name) noexcept;
+
+std::optional<std::vector<std::string>>
+read_file_with_delimeter(std::string_view file_name,
+                         std::string_view delimeter) noexcept;
+
+std::size_t str_to_sizet(const std::string_view str);
