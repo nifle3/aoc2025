7a37c46f79cf7260d5a1843d3f418d17b43c4a29
nifle3 <nifle3123@gmail.com>
2025-12-07 04:58:15 +0300

    complete part2 day5

--- day5/part2.cc
+++ day5/part2.cc
@@ -1,80 +1,16 @@
-#include <memory>
 #include <print>
 #include <ranges>
+#include <set>
 
 #include "utils.hpp"
 
-struct TreeNode {
-  std::uint64_t end;
-  std::uint64_t start;
-  std::uint64_t max_end;
+struct Interval {
+  std::uint64_t l;
+  std::uint64_t r;
 
-  std::shared_ptr<TreeNode> left;
-  std::shared_ptr<TreeNode> right;
+  Interval(std::uint64_t l, std::uint64_t r) : l(l), r(r) {}
 
-  TreeNode(std::uint64_t start, std::uint64_t end) noexcept
-      : end(end), start(start), max_end(end), left(nullptr), right(nullptr) {}
-
-  void add(std::shared_ptr<TreeNode> new_node) noexcept {
-    this->max_end = std::max(this->max_end, new_node->end);
-
-    if (this->start <= new_node->start) {
-      if (!this->right) {
-        this->right = new_node;
-        return;
-      }
-
-      this->right->add(new_node);
-    } else {
-      if (!this->left) {
-        this->left = new_node;
-        return;
-      }
-
-      this->left->add(new_node);
-    }
-  }
-  bool find(std::uint64_t value) noexcept {
-    if (this->max_end < value) {
-      return false;
-    }
-
-    if (this->end >= value && this->start <= value) {
-      return true;
-    }
-
-    if (this->left && this->left->max_end >= value) {
-      return this->left->find(value);
-    }
-
-    if (this->right && this->start < value) {
-      return this->right->find(value);
-    }
-
-    return false;
-  }
-};
-
-struct Tree {
-  std::shared_ptr<TreeNode> root;
-
-  Tree() noexcept : root(nullptr) {}
-
-  void add(std::shared_ptr<TreeNode> new_node) noexcept {
-    if (!this->root) {
-      this->root = new_node;
-    } else {
-      this->root->add(new_node);
-    }
-  }
-
-  bool find(std::uint64_t value) {
-    if (!this->root) {
-      return false;
-    }
-
-    return this->root->find(value);
-  }
+  bool operator<(const Interval &other) const { return this->l < other.l; }
 };
 
 std::pair<std::string, std::string> split_with(std::string_view str) {
@@ -90,6 +26,30 @@ std::pair<std::string, std::string> split_with(std::string_view str) {
   return std::pair{start, end};
 }
 
+void add_interval(std::set<Interval> &intervals, std::uint64_t l,
+                  std::uint64_t r) {
+  Interval new_interval(l, r);
+  auto it = intervals.lower_bound(new_interval);
+  if (it != intervals.begin()) {
+    it--;
+    if (it->r < new_interval.l) {
+      it++;
+    }
+  }
+
+  while (it != intervals.end()) {
+    if (it->l > new_interval.r) {
+      break;
+    }
+    new_interval.l = std::min(new_interval.l, it->l);
+    new_interval.r = std::max(new_interval.r, it->r);
+
+    it = intervals.erase(it);
+  }
+
+  intervals.insert(new_interval);
+}
+
 int main() {
   auto content = get_lines_from_file("./build/day5/input.txt");
   if (!content.has_value()) {
@@ -102,24 +62,19 @@ int main() {
   std::vector<std::string> ids_range(data.begin(), it);
   std::vector<std::string> ids(it != data.end() ? it + 1 : it, data.end());
 
-  Tree tree;
+  std::set<Interval> intervals;
 
   for (auto &&range : ids_range) {
     const auto [start, end] = split_with(range);
     const auto starti = std::stoull(std::move(start));
     const auto endi = std::stoull(std::move(end));
-    const auto new_node = std::make_shared<TreeNode>(starti, endi);
-    tree.add(new_node);
+    add_interval(intervals, starti, endi);
   }
 
   std::size_t result(0);
 
-  for (auto &&id : ids) {
-    const auto idi = std::stoull(std::move(id));
-    const auto is_find = tree.find(idi);
-    if (is_find) {
-      result++;
-    }
+  for (auto &&interval : intervals) {
+    result += interval.r - interval.l + 1;
   }
 
   std::println("Result: {}", result);
